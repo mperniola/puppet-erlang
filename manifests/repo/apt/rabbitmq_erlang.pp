51ab4c5a9d9bd81c13d647e5af5c7f5aa88ffb50
Michele Perniola <michele.perniola@ba.infn.it>
2023-02-21 11:37:04 +0100

    Update rabbitmq_erlang.pp

--- manifests/repo/apt/rabbitmq_erlang.pp
+++ manifests/repo/apt/rabbitmq_erlang.pp
@@ -6,7 +6,6 @@ class erlang::repo::apt::rabbitmq_erlang (
   String $release     = downcase($facts['os']['distro']['codename']),
   String $repos       = 'contrib',
   String $key         = '0A9AF2115F4687BD29803A206B73A36E6026DFCA',
-  String $key_source  =  "${location}{downcase($facts['os']['distro']['codename'])}Release.gpg",
   Optional[Variant[Numeric, String]] $pin = $erlang::package_apt_pin,
 ) inherits erlang {
   apt::source { 'erlang-rabbitmq_erlang':
@@ -16,7 +15,6 @@ class erlang::repo::apt::rabbitmq_erlang (
     repos    => $repos,
     key      => {
       'id'     => $key,
-      'source' => $key_source,
     },
   }
 
